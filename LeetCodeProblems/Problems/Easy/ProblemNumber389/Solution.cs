a40ae009623f207f2607ba0359b74ddf35ad2475
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2023-12-24 12:50:27 +0100

    update

---
+++ LeetCodeProblems/Problems/Easy/ProblemNumber389/Solution.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCodeProblems.Problems.Easy.ProblemNumber389
+{
+    public static class Solution
+    {
+        public static char FindTheDifference(string s, string t)
+        {
+
+            Dictionary<char, int> sDic = new Dictionary<char, int>();
+            Dictionary<char, int> tDic = new Dictionary<char, int>();
+
+            foreach (var item in s)
+            {
+                if (sDic.ContainsKey(item))
+                    sDic[item] = sDic[item] + 1;
+                else
+                    sDic.Add(item, 1);
+
+            }
+            foreach (var item in t)
+            {
+                if (tDic.ContainsKey(item))
+                    tDic[item] = tDic[item] + 1;
+                else
+                    tDic.Add(item, 1);
+
+            }
+
+            foreach (var tDicitem in tDic)
+            {
+                if (sDic.TryGetValue(tDicitem.Key, out int value))
+                {
+                    var sDicitem = value;
+
+                    if (tDicitem.Value > sDicitem)
+                        return tDicitem.Key; 
+                }
+                else
+                    return tDicitem.Key; 
+            }
+
+
+            return default;
+        }
+    }
+}
---
+++ LeetCodeProblems/Problems/Easy/ProblemNumber389/TestCases.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Problems.Easy.ProblemNumber389
+{
+    public static class TestCases
+    {
+        public static bool ExcuteSolution()
+        {
+
+            char outPut1 = Solution.FindTheDifference("abcd", "abcde");
+            if (outPut1 != 'e')
+            {
+                Console.WriteLine("[Problem N389] --> Test Case 1 didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut1}");
+                return false;
+            }
+
+            char outPut2 = Solution.FindTheDifference("", "y");
+            if (outPut2 != 'y')
+            {
+                Console.WriteLine("[Problem N389] --> Test Case y didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut2}");
+                return false;
+            }
+
+            string testCase3Exp1 = "ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwlhvkrgcsithaqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnischrgmtvjcorypvopfmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnhfzcjjcpgzjjfateajnnvlbwhyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddmwlwubetyaummenkdfptjczxemryuotrrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyjxswjwofngpwfxvknkjviiavorwyfzlnktmfwxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwukprgvihntsyymdrfovaszjywuqygpvjtvlsvvqbvzsmgweiayhlubnbsitvfxawhfmfiatxvqrcwjshvovxknnxnyyfexqycrlyksderlqarqhkxyaqwlwoqcribumrqjtelhwdvaiysgjlvksrfvjlcaiwrirtkkxbwgicyhvakxgdjwnwmubkiazdjkfmotglclqndqjxethoutvjchjbkoasnnfbgrnycucfpeovruguzumgmgddqwjgdvaujhyqsqtoexmnfuluaqbxoofvotvfoiexbnprrxptchmlctzgqtkivsilwgwgvpidpvasurraqfkcmxhdapjrlrnkbklwkrvoaziznlpor"; 
+            string testCase3Exp2 = "qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcvvjoggecjsajbuqkjjxfvynrjsnvtfvgpgveycxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfetcdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqpqbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgrorudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbkvpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiyldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj"; 
+
+            char outPut3 = Solution.FindTheDifference(testCase3Exp1, testCase3Exp2);
+            if (outPut3 != 't')
+            {
+                Console.WriteLine("[Problem N389] --> Test Case y didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut3}");
+                return false;
+            }
+
+
+
+            return true;
+        }
+    }
+}
