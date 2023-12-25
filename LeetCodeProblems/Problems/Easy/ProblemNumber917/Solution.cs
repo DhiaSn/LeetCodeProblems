e34c9dcde02fef0ae5d55853e0627c6d472e2e6b
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2023-12-25 12:37:29 +0100

    Solve Problem Number 917 - Easy

---
+++ LeetCodeProblems/Problems/Easy/ProblemNumber917/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Problems.Easy.ProblemNumber917
+{
+    public static class Solution
+    {
+        public static string ReverseOnlyLetters(string s)
+        {
+            string englishLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            StringBuilder result = new(s.Length);
+
+            Dictionary<int, char> specialCharacter = new Dictionary<int, char>();
+
+            for (int i = s.Length - 1; i >= 0; i--) 
+            {
+                if (englishLetters.Contains($"{s[i]}", StringComparison.CurrentCultureIgnoreCase))
+                    result.Append(s[i]);
+                else
+                    specialCharacter.Add(i, s[i]); 
+            }
+            specialCharacter = specialCharacter.OrderBy(x => x.Key).ToDictionary();
+             
+            foreach (var sC in specialCharacter)
+            {
+                result.Insert(sC.Key, sC.Value);
+            }
+
+            return result.ToString(); 
+        }
+    }
+}
---
+++ LeetCodeProblems/Problems/Easy/ProblemNumber917/TestCases.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Problems.Easy.ProblemNumber917
+{
+    public static class TestCases
+    {
+        public static bool ExcuteSolution()
+        {
+            string outPut1 = Solution.ReverseOnlyLetters("ab-cd");
+            if (outPut1 != "dc-ba")
+            {
+                Console.WriteLine("[Problem N389] --> Test Case 1 didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut1}");
+                return false;
+            }
+
+            string outPut2 = Solution.ReverseOnlyLetters("a-bC-dEf-ghIj");
+            if (outPut2 != "j-Ih-gfE-dCba")
+            {
+                Console.WriteLine("[Problem N389] --> Test Case 2 didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut2}");
+                return false;
+            }
+
+            string outPut3 = Solution.ReverseOnlyLetters("Test1ng-Leet=code-Q!");
+            if (outPut3 != "Qedo1ct-eeLg=ntse-T!")
+            {
+                Console.WriteLine("[Problem N389] --> Test Case 3 didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut3}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
