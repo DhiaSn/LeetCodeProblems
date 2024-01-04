cd2e7ee1972873057804b167eb0c0eb26395c8c3
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2024-01-04 11:14:35 +0100

    Solve Problem Number 2351 - Easy

---
+++ LeetCodeProblems/Problems/Easy/ProblemNumber2351/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Problems.Easy.ProblemNumber2351
+{
+    public static class Solution
+    {
+        public static char RepeatedCharacter(string s)
+        {
+            Dictionary<char, int> keyValuePairs = [];
+            foreach (char c in s)
+                if (!keyValuePairs.TryAdd(c, 1))
+                    return c;
+            return ' ';
+        }
+    }
+}
---
+++ LeetCodeProblems/Problems/Easy/ProblemNumber2351/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Problems.Easy.ProblemNumber2351
+{
+    public static class TestCases
+    {
+        public static bool ExcuteSolution()
+        {
+            char outPut1 = Solution.RepeatedCharacter("abccbaacz");
+            if (outPut1 != 'c')
+            {
+                Console.WriteLine("[Problem N389] --> Test Case 1 didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut1}");
+                return false;
+            }
+
+            char outPut2 = Solution.RepeatedCharacter("abcdd");
+            if (outPut2 != 'd')
+            {
+                Console.WriteLine("[Problem N389] --> Test Case 2 didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut2}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
