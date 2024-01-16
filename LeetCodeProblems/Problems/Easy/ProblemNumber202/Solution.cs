fbef04343643f8e03c85e9017e90359ebde3f598
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2024-01-16 13:03:40 +0100

    Solve Problem Number 202 - Easy

---
+++ LeetCodeProblems/Problems/Easy/ProblemNumber202/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Problems.Easy.ProblemNumber202
+{
+    public static class Solution
+    {
+        public static bool IsHappy(int n)
+        {
+            Stack<int> seen = new();
+
+            while (n != 1 && !seen.Contains(n))
+            {
+                seen.Push(n);
+                int result = 0;
+
+                while (n > 0)
+                {
+                    int digit = n % 10;
+                    result += digit * digit;
+                    n /= 10;
+                }
+
+                n = result;
+            }
+
+            return n == 1;
+        }
+    }
+}
---
+++ LeetCodeProblems/Problems/Easy/ProblemNumber202/TestCases.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Problems.Easy.ProblemNumber202
+{
+    public static class TestCases
+    {
+        public static bool ExcuteSolution()
+        {
+            bool outPut1 = Solution.IsHappy(19);
+            if (outPut1 != true)
+            {
+                Console.WriteLine("[Problem N389] --> Test Case 1 didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut1}");
+                return false;
+            }
+            
+            bool outPut2 = Solution.IsHappy(2);
+            if (outPut2 != false)
+            {
+                Console.WriteLine("[Problem N389] --> Test Case 2 didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut2}");
+                return false;
+            }
+            
+            bool outPut3 = Solution.IsHappy(7);
+            if (outPut3 != true)
+            {
+                Console.WriteLine("[Problem N389] --> Test Case 3 didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut3}");
+                return false;
+            }
+
+            bool outPut4 = Solution.IsHappy(5);
+            if (outPut4 != false)
+            {
+                Console.WriteLine("[Problem N389] --> Test Case 4 didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut4}");
+                return false;
+            }
+
+            bool outPut5 = Solution.IsHappy(59);
+            if (outPut5 != false)
+            {
+                Console.WriteLine("[Problem N389] --> Test Case 5 didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut5}");
+                return false;
+            }
+            bool outPut6 = Solution.IsHappy(99);
+            if (outPut6 != false)
+            {
+                Console.WriteLine("[Problem N389] --> Test Case 6 didn't work correctly!");
+                Console.WriteLine($"[Problem N389] --> OutPut = {outPut6}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
